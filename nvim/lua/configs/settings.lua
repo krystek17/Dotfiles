7d8506a068635d448e672c3393ba7305b4759ef7
krystek <90040861+krystek17@users.noreply.github.com>
2022-01-21 21:20:04 +0100

    nvim

---
+++ nvim/lua/configs/settings.lua
@@ -0,0 +1,22 @@
+local options = {
+	number = true,
+	ignorecase = true,
+	mouse = "a",
+	swapfile = false,
+	termguicolors = true,
+	smartindent = true,
+	smartcase = true,
+	cursorline = true,
+	fileencoding = "utf-8",
+	clipboard = "unnamedplus",
+	hlsearch = true,
+	undofile = true,
+	tabstop = 2,
+  cmdheight = 2,
+  shiftwidth = 2,
+	expandtab = true,
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
