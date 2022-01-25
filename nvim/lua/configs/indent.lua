3c573e3ec1e311e9d12f4cf49b4af60c75caa812
krystek <90040861+krystek17@users.noreply.github.com>
2022-01-25 12:17:24 +0100

    indent

--- nvim/lua/configs/indent.lua
+++ nvim/lua/configs/indent.lua
@@ -1,3 +1,15 @@
+
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_filetype_exclude = {
+	"help",
+	"startify",
+	"dashboard",
+	"packer",
+	"neogitstatus",
+	"NvimTree",
+	"Trouble",
+}
+
 vim.opt.termguicolors = true
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
